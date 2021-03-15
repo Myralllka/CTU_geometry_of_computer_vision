9a7573d1263a00c162bc0f30ca65194ed920cc07
Mykola Morgunenko <Olli25010@gmail.com>
2021-03-15 15:56:42 +0100

    correct hw3

--- hw3/hw03.py
+++ hw3/hw03.py
@@ -4,6 +4,7 @@ import scipy.io as sio  # for matlab file format output
 import itertools  # for generating all combinations
 from hw02 import estimate_Q
 from mpl_toolkits import mplot3d
+import scipy.linalg as slinalg
 
 
 def Q2KRC(Q: np.ndarray):
@@ -23,15 +24,13 @@ def Q2KRC(Q: np.ndarray):
     K[0, 2] = M[0] @ M[2] / m3sq
     K[1, 1] = np.sqrt(((M[1] @ M[1]) / m3sq) - K[1, 2] ** 2)
     K[0, 1] = (((M[0] @ M[1]) / m3sq) - (K[0, 2] * K[1, 2])) / K[1, 1]
-    K[0, 0] = np.sqrt((M[1] @ M[1] / m3sq) - K[0, 1] ** 2 - K[0, 2] ** 2)
+    K[0, 0] = np.sqrt((M[0] @ M[0] / m3sq) - K[0, 1] ** 2 - K[0, 2] ** 2)
     K[2, 2] = 1
-    R = np.linalg.inv(K) @ (np.sign(np.linalg.det(M)) / np.linalg.norm(M[2])
-                            * M)
-    # print(R)
-    # print(np.linalg.det(R))
-    # print(R.T @ R)
-    C = -np.linalg.inv(M) @ m
+    s = 1 if np.linalg.det(M) > 0 else -1
+    R = np.linalg.inv(K) @ (
+            np.sign(np.linalg.det(M)) * M / np.linalg.norm(M[2]))
 
+    C = -np.linalg.inv(M) @ m
     return K, R, C
 
 
@@ -46,27 +45,28 @@ def plot_csystem(ax, base, origin, name, color):
     :param base: 3x2 or 3x3 mat
     :param origin: 1x3 vec
     """
-    ax.quiver3D(origin[0], origin[1], origin[2],
+    ax.quiver3D(origin[0, 0], origin[1, 0], origin[2, 0],
                 base[0][0],
                 base[1][0],
                 base[2][0],
                 arrow_length_ratio=0.1,
                 color=color)
-    ax.text(base[0][0] + origin[0],
-            base[1][0] + origin[1],
-            base[2][0] + origin[2],
+    ax.text(base[0][0] + origin[0, 0],
+            base[1][0] + origin[1, 0],
+            base[2][0] + origin[2, 0],
             name + "_x")
     ax.quiver3D(origin[0], origin[1], origin[2],
-                base[0][1] - origin[0],
-                base[1][1] - origin[1],
-                base[2][1] - origin[2],
+                base[0][1] - origin[0, 0],
+                base[1][1] - origin[1, 0],
+                base[2][1] - origin[2, 0],
                 arrow_length_ratio=0.1,
                 color=color)
     ax.text(base[0][1], base[1][1], base[2][1], name + "_y")
     if base.shape[1] > 2:
-        ax.quiver3D(origin[0], origin[1], origin[2],
-                    base[0][2] - origin[0], base[1][2] - origin[1], base[2][
-                        2] - origin[2],
+        ax.quiver3D(origin[0, 0], origin[1, 0], origin[2, 0],
+                    base[0][2] - origin[0, 0],
+                    base[1][2] - origin[1, 0],
+                    base[2][2] - origin[2, 0],
                     arrow_length_ratio=0.1,
                     color=color)
         ax.text(base[0][2], base[1][2], base[2][2], name + "_z")
@@ -81,7 +81,6 @@ if __name__ == "__main__":
     ix = np.array([86, 77, 83, 7, 20, 45, 63, 74, 26, 38])
 
     Q, points_sel, err_max, err_points, Q_all = estimate_Q(u, x, ix)
-    # Q = np.array([[1, 0, 1, 1], [0, 1, 1, 1], [0, 0, 1, 1]])
     K, R, C = Q2KRC(Q)
 
     b1 = 5 * 10e-6
@@ -91,25 +90,34 @@ if __name__ == "__main__":
 
     Delta = np.eye(3)
     d = np.array([0, 0, 0])
+    d = np.array([[d[0]], [d[1]], [d[2]]])
 
     Epsilon = Delta @ np.linalg.inv(R)
     e = C.copy()
+    e = np.array([[e[0]], [e[1]], [e[2]]])
 
     Nu = Epsilon @ np.linalg.inv(K)
     n = C.copy()
+    n = np.array([[n[0]], [n[1]], [n[2]]])
 
     Kappa = Delta * f
     k = d.copy()
+    # k = np.array([[k[0]], [k[1]], [k[2]]])
 
     Gamma = Kappa @ np.linalg.inv(R)
     g = C.copy()
+    g = np.array([[g[0]], [g[1]], [g[2]]])
 
     Beta = Nu * f
     b = C.copy()
+    b = np.array([[b[0]], [b[1]], [b[2]]])
 
     Alpha = Beta @ np.array([[1, 0], [0, 1], [0, 0]])
     a = C + Beta[2]
+    a = np.array([[a[0]], [a[1]], [a[2]]])
 
+    # for i in (a, b, g, d, e, k, n):
+    #     print(i.shape)
     sio.savemat('03_bases.mat', {
         'Pb': Pb, 'f': f,
         'Alpha': Alpha, 'a': a,
@@ -120,32 +128,14 @@ if __name__ == "__main__":
         'Kappa': Kappa, 'k': k,
         'Nu': Nu, 'n': n
         })
-    a.reshape(3, 1)
-    print(a)
-    # print(a.shape)
-    # print(b.shape)
-    # print(g.shape)
-    # print(d.shape)
-    # print(e.shape)
-    # print(k.shape)
-    # print(n.shape)
-    # print(Alpha.shape)
-    # print(Beta.shape)
-    # print(Gamma.shape)
-    # print(Delta.shape)
-    # print(Epsilon.shape)
-    # print(Kappa.shape)
-    # print(Nu.shape)
-    # print(Pb.shape)
-
-    tmp = sio.loadmat("03_bases.mat")
-    print(tmp)
-
-    # print(np.linalg.det(R))
-
+    #
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     # Task 1
+    # borders = 10
+    # ax.axes.set_xlim3d(left=-borders, right=borders)
+    # ax.axes.set_ylim3d(bottom=-borders, top=borders)
+    # ax.axes.set_zlim3d(bottom=-borders, top=borders)
     Beta = Beta @ np.array([[1100, 0, 0], [0, 850, 0], [0, 0, 1]])
     Alpha = Alpha @ np.array([[1100, 0], [0, 850]])
     plot_csystem(ax, Delta, d, 'δ', 'black')
@@ -154,18 +144,22 @@ if __name__ == "__main__":
     plot_csystem(ax, Kappa, k, 'κ', 'brown')
     plot_csystem(ax, Nu, n, 'υ', 'cyan')
     ax.plot3D(x[0], x[1], x[2], "b.")
-    # fig.savefig("03_figure1.pdf")
+    fig.savefig("03_figure1.pdf")
 
     # Task 2
+    # borders = 10
+    # ax.axes.set_xlim3d(left=-borders, right=borders)
+    # ax.axes.set_ylim3d(bottom=-borders, top=borders)
+    # ax.axes.set_zlim3d(bottom=-borders, top=borders)
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     plot_csystem(ax, Beta, b, 'β', 'red')
     plot_csystem(ax, Gamma, g, 'γ', 'blue')
     plot_csystem(ax, Alpha, a, 'α', 'green')
     ax.plot3D(x[0], x[1], x[2], 'b.')
-    # fig.savefig("03_figure2.pdf")
+    fig.savefig("03_figure2.pdf")
 
-    # Task 3
+    # # Task 3
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     borders = 1
@@ -178,5 +172,5 @@ if __name__ == "__main__":
     for q in Q_all:
         K, R, C = Q2KRC(q[0])
         ax.plot3D(C[0], C[1], C[2], 'r.')
-    # plt.show()
-    # fig.savefig("03_figure3.pdf")
+    plt.show()
+    fig.savefig("03_figure3.pdf")
