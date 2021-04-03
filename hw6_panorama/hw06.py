d61d2788d458f3f7b872701991f8262c872839b6
Mykola Morgunenko <Olli25010@gmail.com>
2021-04-04 00:31:20 +0200

    started hw6

---
+++ hw6_panorama/hw06.py
@@ -0,0 +1,9 @@
+import numpy as np  # for matrix computation and linear algebra
+import matplotlib.pyplot as plt  # for drawing and image I/O
+import scipy.io as sio  # for matlab file format output
+import itertools  # for generating all combinations
+import scipy.linalg as slinalg
+
+
+if __name__ == "__main__":
+    pass
