bf315d9183cb92868ba19bb15f459d3f9867e071
Mykola Morgunenko <Olli25010@gmail.com>
2021-05-09 09:37:16 +0200

    started u2F

--- hw8_epipolar_geometry/hw08.py
+++ hw8_epipolar_geometry/hw08.py
@@ -9,15 +9,42 @@ def u2F(u1, u2):
     # computes the fundamental matrix using the seven-point algorithm from 7
     # euclidean correspondences u1, u2, measured in two images. For
     # constructing the third order polynomial from null space matrices G1 and G2
-    print(u1)
     for inx in itertools.combinations(range(0, 12), 7):
         u1_current = u1[:, inx]
         u2_current = u2[:, inx]
-        print(inx)
-        u2F_polynom()
+        tmp_G = list()
+        for counter in range(7):
+            a1 = np.array([u1_current[0][counter], u1_current[1][counter], 1])
+            a2 = np.array([u2_current[0][counter], u2_current[1][counter], 1])
+            tmp_G.append(np.c_[a2[0] * a1[0], a2[0] * a1[1], a2[0] * a1[2],
+                               a2[1] * a1[0], a2[1] * a1[1], a2[1] * a2[2],
+                               a2[2] * a1[0], a2[2] * a1[1], a2[2] * a1[2]])
+        tmp_G = np.array(tmp_G)
+        tmp_G = tmp_G.reshape(7, 9)
+        result = scipy.linalg.null_space(tmp_G)
+        G1 = result.T[0].reshape(3, 3)
+        G2 = result.T[1].reshape(3, 3)
+        polynom = u2F_polynom(G1, G2)
+        roots = np.roots(polynom)
+        for root in roots:
+            if np.iscomplex(root):
+                continue
+            else:
+                G = G1 + root * G2
+                if np.array_equal(G, G2):
+                    continue
+                else:
+                    for counter in range(12):
+                        a1 = np.array([u1[0][counter],
+                                       u1[1][counter], 1])
+                        a2 = np.array([u2[0][counter],
+                                       u2[1][counter], 1])
+                        ep1 = G @ a1
+                        ep2 = G.T @ a2
+                        # print(ep1)
+                        # print(ep2)
 
-
-def u2F_polynom(g1: np.array, g2: np.array):
+def u2F_polynom(q: np.array, p: np.array):
     # %U2F_POLYNOM  Coefficients of polynomial for 7-point algorithm
     # %
     # %   [ a0, a1, a2, a3 ] = u2f_polynom( G1, G2 )
@@ -25,29 +52,27 @@ def u2F_polynom(g1: np.array, g2: np.array):
     # % (c) 2020-04-14 Martin Matousek
     # % Last change: $Date$
     # %              $Revision$
+    # ".replace("1", "0").replace("2", "1").replace("3", "2")
+    a3 = np.linalg.det(p)
 
-    a3 = np.linalg.det(g2)
+    a2 = p[1, 0] * p[2, 1] * q[0, 2] - p[1, 0] * p[0, 1] * q[2, 2] + p[0, 0] * \
+         p[1, 1] * q[2, 2] + p[2, 0] * q[0, 1] * p[1, 2] + p[2, 0] * p[0, 1] * \
+         q[1, 2] - p[0, 0] * q[2, 1] * p[1, 2] - p[2, 0] * q[1, 1] * p[0, 2] - \
+         p[2, 0] * p[1, 1] * q[0, 2] - p[0, 0] * p[2, 1] * q[1, 2] + q[1, 0] * \
+         p[2, 1] * p[0, 2] + p[1, 0] * q[2, 1] * p[0, 2] + q[2, 0] * p[0, 1] * \
+         p[1, 2] - q[1, 0] * p[0, 1] * p[2, 2] - q[0, 0] * p[1, 2] * p[2, 1] - \
+         p[1, 0] * q[0, 1] * p[2, 2] + p[0, 0] * q[1, 1] * p[2, 2] + q[0, 0] * \
+         p[1, 1] * p[2, 2] - q[2, 0] * p[1, 1] * p[0, 2]
+    a1 = q[0, 0] * q[1, 1] * p[2, 2] + q[0, 0] * p[1, 1] * q[2, 2] + p[2, 0] * \
+         q[0, 1] * q[1, 2] - q[1, 0] * q[0, 1] * p[2, 2] - p[0, 0] * q[2, 1] * \
+         q[1, 2] - p[1, 0] * q[0, 1] * q[2, 2] - p[2, 0] * q[1, 1] * q[0, 2] + \
+         p[0, 0] * q[1, 1] * q[2, 2] + q[1, 0] * q[2, 1] * p[0, 2] + q[1, 0] * \
+         p[2, 1] * q[0, 2] + q[2, 0] * p[0, 1] * q[1, 2] - q[1, 0] * p[0, 1] * \
+         q[2, 2] - q[2, 0] * p[1, 1] * q[0, 2] + p[1, 0] * q[2, 1] * q[0, 2] - \
+         q[0, 0] * p[2, 1] * q[1, 2] - q[2, 0] * q[1, 1] * p[0, 2] + q[2, 0] * \
+         q[0, 1] * p[1, 2] - q[0, 0] * q[2, 1] * p[1, 2]
 
-    a2 = g2[2, 1] * g2[3, 2] * g1[1, 3] - g2[2, 1] * g2[1, 2] * g1[3, 3] \
-         + g2[1, 1] * g2[2, 2] * g1[3, 3] + g2[3, 1] * g1[1, 2] * g2[2, 3] \
-         + g2[3, 1] * g2[1, 2] * g1[2, 3] - g2[1, 1] * g1[3, 2] * g2[2, 3] \
-         - g2[3, 1] * g1[2, 2] * g2[1, 3] - g2[3, 1] * g2[2, 2] * g1[1, 3] \
-         - g2[1, 1] * g2[3, 2] * g1[2, 3] + g1[2, 1] * g2[3, 2] * g2[1, 3] \
-         + g2[2, 1] * g1[3, 2] * g2[1, 3] + g1[3, 1] * g2[1, 2] * g2[2, 3] \
-         - g1[2, 1] * g2[1, 2] * g2[3, 3] - g1[1, 1] * g2[2, 3] * g2[3, 2] \
-         - g2[2, 1] * g1[1, 2] * g2[3, 3] + g2[1, 1] * g1[2, 2] * g2[3, 3] \
-         + g1[1, 1] * g2[2, 2] * g2[3, 3] - g1[3, 1] * g2[2, 2] * g2[1, 3]
-
-    a1 = + g1[1, 1] * g1[2, 2] * g2[3, 3] + g1[1, 1] * g2[2, 2] * g1[3, 3] \
-         + g2[3, 1] * g1[1, 2] * g1[2, 3] - g1[2, 1] * g1[1, 2] * g2[3, 3] \
-         - g2[1, 1] * g1[3, 2] * g1[2, 3] - g2[2, 1] * g1[1, 2] * g1[3, 3] \
-         - g2[3, 1] * g1[2, 2] * g1[1, 3] + g2[1, 1] * g1[2, 2] * g1[3, 3] \
-         + g1[2, 1] * g1[3, 2] * g2[1, 3] + g1[2, 1] * g2[3, 2] * g1[1, 3] \
-         + g1[3, 1] * g2[1, 2] * g1[2, 3] - g1[2, 1] * g2[1, 2] * g1[3, 3] \
-         - g1[3, 1] * g2[2, 2] * g1[1, 3] + g2[2, 1] * g1[3, 2] * g1[1, 3] \
-         - g1[1, 1] * g2[3, 2] * g1[2, 3] - g1[3, 1] * g1[2, 2] * g2[1, 3] \
-         + g1[3, 1] * g1[1, 2] * g2[2, 3] - g1[1, 1] * g1[3, 2] * g2[2, 3]
-    a0 = np.linalg.det(g1)
+    a0 = np.linalg.det(q)
     return [a0, a1, a2, a3]
 
 
@@ -57,13 +82,14 @@ if __name__ == "__main__":
     f = sio.loadmat("daliborka_01_23-uu.mat")
 
     # indices of a points, that form an edge
-    edges = f["edges"]
+    edges = f["edges"]  # - 1
     # list of 12 indices of points ix
-    ix = f["ix"][0]
+    ix = f["ix"][0] - 1
     # There is a set of point matches between the images above.
     u01 = f["u01"]
     u23 = f["u23"]
-
+    u01 = np.array([u01[0] - 1, u01[1] - 1])
+    u23 = np.array([u23[0] - 1, u23[1] - 1])
     # Step 1. Find the fundamental matrix F relating the images above: generate
     # all 7-tuples from the selected set of 12 correspondences, estimate F for
     # each of them and chose the one, that minimizes maximal epipolar error
@@ -73,7 +99,7 @@ if __name__ == "__main__":
     u2 = u23[:, ix]
 
     F = u2F(u1, u2)
-    
+
     #  Step 2. Draw the 12 corresponding points in different colour in the two
     #  images. Using the best F, compute the corresponding epipolar lines and
     #  draw them into the images in corresponding colours (a line segment given
