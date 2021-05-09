85d9144d50e55192452332c40f624065c679c9c2
Mykola Morgunenko <Olli25010@gmail.com>
2021-05-09 17:16:59 +0200

    almost complete the task

--- hw8_epipolar_geometry/hw08.py
+++ hw8_epipolar_geometry/hw08.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt  # for drawing and image I/O
 import scipy.io as sio  # for matlab file format output
 import itertools  # for generating all combinations
 import scipy.linalg
+import math
 
 ixs = []
 
 
 def shortest_distance(p, lnn) -> float:
-    return np.abs((lnn[0] * p[0] + lnn[1] * p[1] + lnn[2])) / (
-        np.sqrt(lnn[0] ** 2 + lnn[1] ** 2))
+    return abs((lnn[0] * p[0] + lnn[1] * p[1] + lnn[2])) / (math.sqrt(lnn[0] ** 2 + lnn[1] ** 2))
 
 
 def u2F(u1, u2):
@@ -17,7 +17,9 @@ def u2F(u1, u2):
     # euclidean correspondences u1, u2, measured in two images. For
     # constructing the third order polynomial from null space matrices G1 and G2
     result_F_errors = []
-    for inx in itertools.combinations(range(0, 12), 7):
+    # u1 = u1.reshape(2, 14)
+    # u2 = u2.reshape(2, 14)
+    for inx in itertools.combinations(range(0, len(u1[0])), 7):
         u1_current = u1[:, inx]
         u2_current = u2[:, inx]
         tmp_G = list()
@@ -42,24 +44,22 @@ def u2F(u1, u2):
             if np.iscomplex(root):
                 continue
             root = np.real(root)
-            d1_i, d2_i = 0, 0
             G = G1 + root * G2
             if np.array_equal(G, G2):
                 continue
             if np.linalg.matrix_rank(G) != 2:
                 continue
             point_errors = []
-            for counter in range(12):
+            for counter in range(len(u1[0])):
                 a1 = np.array([u1[0][counter],
                                u1[1][counter], 1])
                 a2 = np.array([u2[0][counter],
                                u2[1][counter], 1])
                 ep1 = G.T @ a2
                 ep2 = G @ a1
-                d1_i += shortest_distance(a1, ep1)
-                d2_i += shortest_distance(a2, ep2)
-                point_errors.append(shortest_distance(a1, ep1) +
-                                    shortest_distance(a2, ep2))
+                d1_i = shortest_distance(a1, ep1)
+                d2_i = shortest_distance(a2, ep2)
+                point_errors.append(d1_i + d2_i)
                 result_FFs.append(G)
                 max_error.append(max(point_errors))
         result_F_errors.append([max(max_error), result_FFs, inx])
@@ -131,10 +131,14 @@ if __name__ == "__main__":
     #  Export as 08_eg.pdf.
     fig = plt.figure()
     fig.clf()
-
     colors = ["dimgray", "rosybrown", "maroon", "peru",
               "moccasin", "yellow", "olivedrab", "lightgreen",
               "navy", "royalblue", "indigo", "hotpink"]
+
+    fig.suptitle('The epipolar lines')
+
+    plt.subplot(121)
+
     i = 0
     for x_p1, y_p1, x_p2, y_p2 in zip(u1[0], u1[1], u2[0], u2[1]):
         plt.plot([int(x_p1)], [int(y_p1)], color=colors[i], marker="X",
@@ -142,20 +146,34 @@ if __name__ == "__main__":
         point1 = np.c_[x_p1, y_p1, 1].reshape(3, 1)
         point2 = np.c_[x_p2, y_p2, 1].reshape(3, 1)
 
-        x = np.linspace(1, 1200, 1)
+        x = np.linspace(1, 1200, 1200)
         ep1 = F[0].T @ point2
+        y = -((ep1[2] / ep1[1]) + x * ep1[0] / ep1[1])
+        plt.plot(x, y, color=colors[i])
+        i += 1
+    plt.imshow(img1)
 
+    plt.subplot(122)
 
-        # point = point.reshape(1,3)
-        # print(point @ F)
+    i = 0
+    for x_p1, y_p1, x_p2, y_p2 in zip(u1[0], u1[1], u2[0], u2[1]):
+        plt.plot([int(x_p2)], [int(y_p2)],
+                 color=colors[i],
+                 marker="X",
+                 markersize=10)
+        point1 = np.c_[x_p1, y_p1, 1].reshape(3, 1)
+        point2 = np.c_[x_p2, y_p2, 1].reshape(3, 1)
 
-        # y = (ep1[1] / ep1[2]) - x * ep1[0] / ep1[2]
+        x = np.linspace(1, 1200, 1200)
+        point1 = point1.reshape(3, 1)
+        ep2 = F[0] @ point1
+        y = -((ep2[2] / ep2[1]) + x * ep2[0] / ep2[1])
+        plt.plot(x, y, color=colors[i])
+        i += 1
+    plt.imshow(img2)
 
-        # plt.plot(x, y, 'r-')
-        # i += 1
-    # plt.imshow(img1)
-    # plt.show()
-
+    plt.show()
+    # fig.savefig("08_eg.pdf")
     # Step 3. Draw graphs of epipolar errors d1_i and d2_i for all points
     # (point index on horizontal axis, the error on vertical axis). Draw both
     # graphs into single figure (different colours) and export as 08_errors.pdf
@@ -185,9 +203,10 @@ if __name__ == "__main__":
     # the indices of the 7 points used for computing the optimal F as point_sel
     # and the matrix F.
 
-    sio.savemat('08_data.mat', {
-        'u1': u01,
-        'u2': u23,
-        "ix_res": [ix[i] for i in range(12) if i in ixs],
-        'F': F[0]
-        })
+    # sio.savemat('08_data.mat', {
+    #     'u1': u01,
+    #     'u2': u23,
+    #     'ix': ix,
+    #     "point_sel": [ix[i] for i in range(12) if i in ixs],
+    #     'F': F[0]
+    #     })
