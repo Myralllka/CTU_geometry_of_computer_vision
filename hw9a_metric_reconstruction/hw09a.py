962f2e2cab86c4d1faeb884c9ef8b5fe04c85d81
Mykola Morgunenko <Olli25010@gmail.com>
2021-05-12 21:41:47 +0200

    started step4

--- hw9a_metric_reconstruction/hw09a.py
+++ hw9a_metric_reconstruction/hw09a.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt  # for drawing and image I/O
 import scipy.io as sio  # for matlab file format output
 import itertools  # for generating all combinations
 import scipy.linalg as slinalg
+from hw08 import u2F
 
 
 def shortest_distance(p, lnn) -> float:
@@ -10,21 +11,22 @@ def shortest_distance(p, lnn) -> float:
         np.sqrt(lnn[0] ** 2 + lnn[1] ** 2))
 
 
-def step2(u1, u2):
+def step2(u1_step2, u2_step2, F_step2):
     fig = plt.figure()
     fig.clf()
     fig.suptitle('The epipolar lines using Fx')
     plt.subplot(121)
 
     i = 0
-    for x_p1, y_p1, x_p2, y_p2 in zip(u1[0], u1[1], u2[0], u2[1]):
+    for x_p1, y_p1, x_p2, y_p2 in zip(u1_step2[0], u1_step2[1], u2_step2[0],
+                                      u2_step2[1]):
         plt.plot([int(x_p1)], [int(y_p1)], color=colors[i], marker="X",
                  markersize=10)
-        point2 = np.c_[x_p2, y_p2, 1].reshape(3, 1)
+        point2_step2 = np.c_[x_p2, y_p2, 1].reshape(3, 1)
 
         x = np.linspace(1, 1200, 1200)
-        ep1 = F.T @ point2
-        y = -((ep1[2] / ep1[1]) + x * ep1[0] / ep1[1])
+        ep1_step2 = F_step2.T @ point2_step2
+        y = -((ep1_step2[2] / ep1_step2[1]) + x * ep1_step2[0] / ep1_step2[1])
         plt.plot(x, y, color=colors[i])
         i += 1
     plt.imshow(img1)
@@ -32,17 +34,18 @@ def step2(u1, u2):
     plt.subplot(122)
 
     i = 0
-    for x_p1, y_p1, x_p2, y_p2 in zip(u1[0], u1[1], u2[0], u2[1]):
+    for x_p1, y_p1, x_p2, y_p2 in zip(u1_step2[0], u1_step2[1], u2_step2[0],
+                                      u2_step2[1]):
         plt.plot([int(x_p2)], [int(y_p2)],
                  color=colors[i],
                  marker="X",
                  markersize=10)
-        point1 = np.c_[x_p1, y_p1, 1].reshape(3, 1)
+        point1_step2 = np.c_[x_p1, y_p1, 1].reshape(3, 1)
 
         x = np.linspace(1, 1200, 1200)
-        point1 = point1.reshape(3, 1)
-        ep2 = F @ point1
-        y = -((ep2[2] / ep2[1]) + x * ep2[0] / ep2[1])
+        point1_step2 = point1_step2.reshape(3, 1)
+        ep2_step2 = F_step2 @ point1_step2
+        y = -((ep2_step2[2] / ep2_step2[1]) + x * ep2_step2[0] / ep2_step2[1])
         plt.plot(x, y, color=colors[i])
         i += 1
     plt.imshow(img2)
@@ -50,19 +53,19 @@ def step2(u1, u2):
     plt.show()
 
 
-def step3(U1, U2, F):
+def step3(U1, U2, F_step3):
     fig = plt.figure()
     fig.clf()
     d1_arr, d2_arr = [], []
     for i in range(len(U1[0])):
-        a1 = np.array([U1[0][i],
-                       U1[1][i], 1])
-        a2 = np.array([U2[0][i],
-                       U2[1][i], 1])
-        ep1 = F.T @ a2
-        ep2 = F @ a1
-        d1_arr.append(shortest_distance(a1, ep1))
-        d2_arr.append(shortest_distance(a2, ep2))
+        a1_step3 = np.array([U1[0][i],
+                             U1[1][i], 1])
+        a2_step3 = np.array([U2[0][i],
+                             U2[1][i], 1])
+        ep1_step3 = F_step3.T @ a2_step3
+        ep2_step3 = F_step3 @ a1_step3
+        d1_arr.append(shortest_distance(a1_step3, ep1_step3))
+        d2_arr.append(shortest_distance(a2_step3, ep2_step3))
     plt.title('The epipolar error for all points')
     plt.xlabel('point index')
     plt.ylabel('epipolar err [px]')
@@ -117,7 +120,7 @@ if __name__ == "__main__":
     # the two images. Using Fx, compute the corresponding epipolar lines and
     # draw them into the images in corresponding colours. Export as 09_egx.pdf.
 
-    step2(u1, u2)
+    step2(u1, u2, Fx)
 
     # Step 3
     # Draw graphs of epipolar errors d1_i and d2_i w.r.t Fx for all points.
@@ -131,8 +134,8 @@ if __name__ == "__main__":
     # respective fundamental matrix Fe consistent with K using the same
     # correspondences:
 
-    ### Generate all 7-tuples from the set of 12 correspondences and estimate f
-    # undamental matrix F for each of them.
+    ### Generate all 7-tuples from the set of 12 correspondences and estimate
+    # fundamental matrix F for each of them.
 
     ### For each tested F, compute essential matrix E using internal
     # calibration K.
@@ -143,8 +146,31 @@ if __name__ == "__main__":
     ### Choose such Fe and E that minimize maximal epipolar error over all
     # matches.
 
+    result_F_errors_inxs = []
+
     for inx in itertools.combinations(range(0, len(U1[0])), 7):
-        pass
+        u1_current = u1[:, inx]
+        u2_current = u2[:, inx]
+        Fs_step4 = u2F(u1_current, u2_current)
+        E_step4 = K.T @ Fs_step4 @ K
+        for each_E in E_step4:
+            point_errors = []
+            for counter in range(len(u01[0])):
+                a1 = np.array([u01[0][counter],
+                               u01[1][counter],
+                               1])
+                a2 = np.array([u23[0][counter],
+                               u23[1][counter],
+                               1])
+                ep1 = each_E.T @ a2
+                ep2 = each_E @ a1
+                d1_i = shortest_distance(a1, ep1)
+                d2_i = shortest_distance(a2, ep2)
+                point_errors.append(d1_i + d2_i)
+            result_F_errors_inxs.append([max(point_errors), each_F, inx])
+    result_F_errors_inxs.sort(key=lambda x: x[0])
+    F = result_F_errors_inxs[0][1]
+
     # Step 5
     # Draw the 12 corresponding points in different colour in the two images.
     # Using Fe, compute the corresponding epipolar lines and draw them into the
