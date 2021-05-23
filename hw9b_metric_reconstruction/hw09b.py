74acd421da362883118c0039be3723a91c6f56f0
Mykola Morgunenko <Olli25010@gmail.com>
2021-05-23 18:09:35 +0200

    reconstruction not working

--- hw9b_metric_reconstruction/hw09b.py
+++ hw9b_metric_reconstruction/hw09b.py
@@ -64,12 +64,12 @@ def step5(u1_c, u2_c, X, P1_c, P2_c):
         reprojected = (reprojected / reprojected[-1])[:-1]
         d1_arr.append(
                 np.linalg.norm(
-                    np.array([u1_c[0][i], u1_c[1][i]]) - reprojected))
+                        np.array([u1_c[0][i], u1_c[1][i]]) - reprojected))
         reprojected = P2_c @ X_loop
         reprojected = (reprojected / reprojected[-1])[:-1]
         d2_arr.append(
                 np.linalg.norm(
-                    np.array([u2_c[0][i], u2_c[1][i]]) - reprojected))
+                        np.array([u2_c[0][i], u2_c[1][i]]) - reprojected))
 
     plt.title("Reprojection errors")
     plt.xlabel('point index')
@@ -132,54 +132,64 @@ def compute_Xs(u_current_1, u_current_2, P1_c, P2_c):
                        np.hstack([np.zeros((3, 1)), v_tmp, -P2_c])])
         _, _, Vh = np.linalg.svd(M)
         res_X.append(Vh[-1, 2:5] / Vh[-1, -1])
+    return triangulation(u_current_1, u_current_2, P1_c, P2_c)
+
+
+def triangulation(u_current_1, u_current_2, P1_c, P2_c):
+    res_X = []
+    p11, p12, p13 = P1_c
+    p21, p22, p23 = P2_c
+    for cntr in range(len(u_current_1[0])):
+        u_tmp = np.array([u_current_1[0][cntr],
+                          u_current_1[1][cntr],
+                          1]).reshape(3, 1)
+        v_tmp = np.array([u_current_2[0][cntr],
+                          u_current_2[1][cntr],
+                          1]).reshape(3, 1)
+        r1 = ((u_tmp[0] * p13) - p11)
+        r2 = ((u_tmp[1] * p13) - p12)
+        r3 = ((v_tmp[0] * p23) - p21)
+        # r4 = ((v_tmp[1] * p23) - p22)
+        r4 = np.array([0, 0, 0, 0])
+        M = np.vstack([r1, r2, r3, r4])
+        # _, _, Vh = np.linalg.svd(M)
+        # tmp = Vh[-1] / Vh[-1, -1]
+        Vh = scipy.linalg.null_space(M).reshape(4,)
+        Vh /= Vh[-1]
+        res_X.append(Vh[:-1])
     return res_X
-
-
-# def triangulation(u_current_1, u_current_2, P1_c, P2_c):
-#     res_X = []
-#     p11, p12, p13 = P1_c
-#     p21, p22, p23 = P2_c
-#     for cntr in range(len(u_current_1[0])):
-#         u_tmp = np.array([u_current_1[0][cntr],
-#                           u_current_1[1][cntr],
-#                           1]).reshape(3, 1)
-#         v_tmp = np.array([u_current_2[0][cntr],
-#                           u_current_2[1][cntr],
-#                           1]).reshape(3, 1)
-#         r1 = ((u_tmp[0] * p13) - p11)
-#         r2 = ((u_tmp[1] * p13) - p12)
-#         r3 = ((v_tmp[0] * p23) - p21)
-#         r4 = ((v_tmp[1] * p23) - p22)
-#         M = np.vstack([r1, r2, r3, r4])
-#         print(M)
-#         _, _, Vh = np.linalg.svd(M)
-#         res_X.append(Vh[-1] / Vh[-1, -1])
-#     return res_X
 
 
 def optimize_rc(R1_current, C1_current, R2_current, C2_current, K_current,
                 u_current_1, u_current_2):
     result_index_R_C_Ps = []
-    # P1_c = np.append(K, np.array([0, 0, 0]).reshape(3, 1), axis=1)
+
     P1_c = np.hstack((np.eye(3), np.zeros((3, 1))))
     for R_loop, C_loop in [[R1_current, C1_current],
                            [R1_current, C2_current],
                            [R2_current, C1_current],
                            [R2_current, C2_current]]:
+        P1_c_K = np.append(K, (-K @ C_loop).reshape(3, 1), axis=1)
         counter = 0
-        P2_c = np.append(K_current @ R_loop,
-                         (- K_current @ R_loop @ C_loop).reshape(3, 1), axis=1)
+        P2_c_K = np.append(K_current @ R_loop,
+                           (- K_current @ R_loop @ C_loop).reshape(3, 1),
+                           axis=1)
+        P2_c = np.hstack((R_loop, C_loop.reshape(-1, 1)))
         X = compute_Xs(u_current_1, u_current_2, P1_c, P2_c)
         for each, i in zip(X, range(len(X))):
             u_tmp = np.array([u_current_1[0][i],
                               u_current_1[1][i], 1]).reshape(3, 1)
             v_tmp = np.array([u_current_2[0][i],
                               u_current_2[1][i], 1]).reshape(3, 1)
-            if (np.dot(each, u_tmp) / (
-                    np.linalg.norm(each) * np.linalg.norm(u_tmp)) > 0) and \
-                    (np.dot(each, v_tmp) / (
-                            np.linalg.norm(each) * np.linalg.norm(v_tmp)) > 0):
+            # check if after reconstruction point in front of both cameras
+            if (np.dot(each, u_tmp) /
+                (np.linalg.norm(each) *
+                 np.linalg.norm(u_tmp)) > 0) and \
+                    (np.dot(each, v_tmp) /
+                     (np.linalg.norm(each) *
+                      np.linalg.norm(v_tmp)) > 0):
                 counter += 1
+
         print(counter)
         result_index_R_C_Ps.append([counter, R_loop, C_loop, P1_c, P2_c])
     return sorted(result_index_R_C_Ps, key=lambda x: x[0])[-1]
@@ -204,6 +214,17 @@ if __name__ == "__main__":
     u23 = f["u23"]
     u01 = np.array([u01[0] - 1, u01[1] - 1])
     u23 = np.array([u23[0] - 1, u23[1] - 1])
+
+    def dv(x):
+        tmp = np.linalg.inv(K) @ np.array([x[0], x[1], 1])
+        tmp = (tmp / tmp[-1])[:-1]
+        return tmp
+
+    u01 = np.array([dv(each) for each in u01.T]).T
+    u23 = np.array([dv(each) for each in u23.T]).T
+    # u01 = (np.linalg.inv(K) @ u01)
+    # u23 = (np.linalg.inv(K) @ u23)
+
 
     # Step 1
     # Decompose the best E into relative rotation R and translation C
